148b0af3910da763b3da78b737d3d49d7d39b1e2
jessica <jessica26lai@gmail.com>
2024-12-22 18:15:13 +0000

    Add project files.

---
+++ Data/BlazorOnlineShopContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlazorOnlineShop.Models;
+
+namespace BlazorOnlineShop.Data
+{
+    public class BlazorOnlineShopContext : DbContext
+    {
+        public BlazorOnlineShopContext (DbContextOptions<BlazorOnlineShopContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlazorOnlineShop.Models.Product> Product { get; set; } = default!;
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorOnlineShop.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public DateOnly AddedDate { get; set; }
+        public DateOnly lastUpdatedDate { get; set; }
+
+        public string? lastUpdatedBy { get; set; }
+
+        public string CategoryID { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal SellingPrice { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Cost { get; set; }
+
+        public string? Description { get; set; }
+
+        public int viewedCount { get; set; }
+    }
+}
