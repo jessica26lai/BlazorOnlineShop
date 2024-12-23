1931a2475103b07cc887e39e1b7d08ac71960607
jessica <jessica26lai@gmail.com>
2024-12-23 23:18:08 +0000

    Added Cart function

---
+++ Models/CartItem.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorOnlineShop.Models
+{
+    public class CartItem
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal ProductPrice { get; set; }
+    }
+}
